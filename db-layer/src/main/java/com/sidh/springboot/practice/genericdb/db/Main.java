2b65d32708f3933cfd5927da8f8f22d6103b72a2
sidharth.r <sidharth.r@lendingkart.com>
2020-05-30 15:39:05 +0530

    rearrange modules and dependencies

--- db-layer/src/main/java/com/sidh/springboot/practice/genericdb/db/Main.java
+++ db-layer/src/main/java/com/sidh/springboot/practice/genericdb/db/Main.java
@@ -1,9 +1,12 @@
 package com.sidh.springboot.practice.genericdb.db;
 
+import com.sidh.springboot.practice.genericdb.dtos.entity.Attribute;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = Attribute.class)
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
--- ui-layer/src/main/java/com/sidh/springboot/practice/genericdb/ui/Main.java
+++ ui-layer/src/main/java/com/sidh/springboot/practice/genericdb/ui/Main.java
@@ -1,16 +1,11 @@
 package com.sidh.springboot.practice.genericdb.ui;
 
-import com.sidh.springboot.practice.genericdb.db.repo.AttributeTypeRepo;
 import com.sidh.springboot.practice.genericdb.dtos.entity.AttributeType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {Main.class, com.sidh.springboot.practice.genericdb.db.Main.class})
-@EnableJpaRepositories(basePackageClasses = {AttributeTypeRepo.class})
 @EntityScan(basePackageClasses = AttributeType.class)
 public class Main {
     public static void main(String[] args) {
